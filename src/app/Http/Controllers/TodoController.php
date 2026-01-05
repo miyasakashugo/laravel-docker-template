0b58452cff519a00156ee4a03e446a76f73007fb
Shugo Miyasaka <miyashu.0721@gmail.com>
2026-01-05 14:46:38 +0900

    DBデータを保存する

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -16,9 +16,26 @@ class TodoController extends Controller
         return view('todo.index', ['todos' => $todos]);
     }
 
+    public function store(Request $request)
+    {
+        // dd('新規作成のルート実行！');
+
+        $content = $request->input('content');
+
+        // dd($content); 
+
+        $todo = new Todo(); 
+    // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
+        $todo->content = $content;
+    // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
+        $todo->save();
+
+        return redirect()->route('todo.index');
+    }
+
     public function create()
     {
-        // dd('新規作成画面のルート実行！');
         return view('todo.create');
     }
+    
 }
